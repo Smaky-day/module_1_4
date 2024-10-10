e56743704f2916b552d672c661780b3cc1fdccde
Smaky-day <05102024smoky@gmail.com>
2024-10-10 14:25:27 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+finish_hmWorks = 12
+completion_time = 1.5
+Course_name = 'Python'
+time_oneTask = completion_time / finish_hmWorks
+print('Курс: ', Course_name + ',', 'всего' + ' задач:' + str(finish_hmWorks) + ',', 'затрачено часов: ', str(completion_time) + ',', 'среднее время выполнения ', time_oneTask, ' часа.')
